046c9d33d375b18540d7b20aecb0e98da6a93ba2
Vorinelf <44244914+Vorinelf@users.noreply.github.com>
2019-04-20 20:40:47 +0300

    commit#1 20.04.2019

---
+++ src/main/java/com/artem/training/Main.java
@@ -0,0 +1,106 @@
+package com.artem.training;
+
+import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+class Main {
+
+
+    public static void main(String[] args) {
+        List<User> noChildren = null;
+
+
+        List<User> childrenAnna = new ArrayList<>();
+        childrenAnna.add(new User(1, 8, "Mike"));
+        childrenAnna.add(new User(2, 4, "Dave"));
+        childrenAnna.add(new User(3, 2, "Nick"));
+        User anna = new User(1, 32, "Anna", childrenAnna);
+
+        List<User> childrenBob = new ArrayList<>();
+        childrenBob.add(new User(1, 5, "Sam"));
+        childrenBob.add(new User(2, 1, "Jack"));
+        childrenBob.add(new User(3, 3, "Tad"));
+        User bob = new User(2, 30, "Bob", childrenBob);
+
+        List<User> childrenMike = new ArrayList<>();
+        childrenMike.add(new User(1, 7, "Kate"));
+        childrenMike.add(new User(2, 1, "Jack"));
+        User mike = new User(3, 31, "Mike", childrenMike);
+
+        List<User> childrenNick = new ArrayList<>();
+        childrenNick.add(new User(1, 10, "Gary"));
+        User nick = new User(4, 34, "Kim", childrenNick);
+
+        List<User> childrenJasica = new ArrayList<>();
+        childrenJasica.add(new User(1, 4, "David"));
+        User jasica = new User(5, 22, "Mike", childrenJasica);
+
+        List<User> childrenMonica = new ArrayList<>();
+        childrenMonica.add(new User(1, 8, "Tim"));
+        User monica = new User(6, 26, "Monica", childrenMonica);
+
+
+        User stiv = new User(7, 20, "Stiv", noChildren);
+
+        User kolin = new User(8, 45, "Kolin", null);
+
+        List<User> childrenRoy = new ArrayList<>();
+        childrenRoy.add(new User(1, 12, "John"));
+        User roy = new User(9, 25, "Roy", childrenRoy);
+
+        List<User> childrenLuk = new ArrayList<>();
+        childrenLuk.add(new User(1, 11, "Kate"));
+        childrenLuk.add(new User(2, 9, "Mary"));
+        childrenLuk.add(new User(3, 6, "Tad"));
+        User luk = new User(10, 30, "Bob", childrenLuk);
+
+
+        List<User> parents = new ArrayList<>();
+        parents.add(anna);
+        parents.add(bob);
+        parents.add(mike);
+        parents.add(nick);
+        parents.add(jasica);
+        parents.add(monica);
+        parents.add(stiv);
+        parents.add(kolin);
+        parents.add(roy);
+        parents.add(luk);
+
+        parents
+                .stream().filter((p) -> p.getChildren() != null).flatMap((p) -> p.getChildren().stream().filter((m) -> m.getAge() > 5))
+                .forEach((p) -> System.out.println(p));
+
+
+        int ageParentsSum = parents
+                .stream().mapToInt((p) -> p.getAge()).sum();
+        System.out.println("1. Сумма возрастов родителей: " + ageParentsSum);
+
+        long parentsAgeOld27 = parents
+                .stream().filter((p) -> p.getAge() > 27).count();
+        System.out.println("2. количество родителей старше 27: " + parentsAgeOld27);
+
+        int countAllChildren = parents
+                .stream().filter((p) -> p.getChildren() != null).mapToInt((p) -> p.getChildren().size()).sum();
+        System.out.println("3. Количество всех детей: " + countAllChildren);
+        int summAgeChildren = parents
+                .stream().filter((p) -> p.getChildren() != null).flatMap((o) -> o.getChildren().stream()).mapToInt((l) -> l.getAge()).sum();
+        System.out.println("4. Сумма возрастов детей: " + summAgeChildren);
+
+
+    }
+
+//        parents
+//                .stream()
+//                .filter((n) -> n.getChildren().stream().filter((p)->p.getAge()>2))
+//                .forEach(System.out::println);
+
+
+}
+
+
---
+++ src/main/java/com/artem/training/User.java
@@ -0,0 +1,55 @@
+package com.artem.training;
+
+import java.util.List;
+
+public class User {
+    public void setChildren(List<User> children) {
+        this.children = children;
+    }
+
+    long id;
+    int age;
+    String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<User> getChildren() {
+        return children;
+    }
+
+    private List<User> children;
+
+
+    public User(long id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public User(long id, int age, String name, List<User> children) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.children = children;
+    }
+
+    public String toString() {
+        return "ID: " + this.getId() + "; " + "Name: " + this.getName() + "; " + "Age: " + this.getAge() + "; " + "Children: " + this.getChildren();
+    }
+
+
+}
+
+
+
+
